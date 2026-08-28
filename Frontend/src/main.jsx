f81965dba87a205c49edce40e166be6e0df98b0e
Tushar Saini <tusharsaini.in@gmail.com>
2026-08-28 23:41:25 +0530

    feat: implement main router configuration with lazy loading and idle prefetching

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -12,7 +12,7 @@ import AdminLayout from "./layout/AdminLayout";
 import GlobalError from "./pages/Error";
 
 import Home from "./pages/Home";
-import RouteProgressBar from "./components/common/RouteProgressBar";
+import SplashScreen from "./components/common/SplashScreen";
 import { routeLazy, prefetchRoute } from "./utils/lazyWithRetry";
 
 // Dynamic Loaders
@@ -164,7 +164,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App>
-        <RouterProvider router={router} fallbackElement={<RouteProgressBar />} />
+        <RouterProvider router={router} fallbackElement={<SplashScreen />} />
       </App>
     </Provider>
   </React.StrictMode>
